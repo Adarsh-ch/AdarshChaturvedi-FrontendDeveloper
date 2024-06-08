ce16b2216c7da087d2d9914326e98d5063d42658
Adarsh-ch <adarshchaturvedi98@gmail.com>
2024-06-08 22:31:17 +0530

    nav bar and work on hero section

--- src/components/ Header.jsx
+++ src/components/ Header.jsx
@@ -3,18 +3,20 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="header">
+      <div className="header-div">
       <div className="logo">AI.GEN</div>
       <nav className="nav">
         <ul>
-          <li><a href="#about">About</a></li>
+          {/* <li><a href="#about">About</a></li> */}
           <li><a href="#features">Features</a></li>
           <li><a href="#roadmap">Roadmap</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><a href="#Tokenomics">Tokenomics</a></li>
         </ul>
       </nav>
+      </div>
       <div className="buttons">
-        <button>Sign In</button>
-        <button>Sign Up</button>
+        <button>Whitepaper</button>
+        <button>Get Started</button>
       </div>
     </header>
   );
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,8 +3,17 @@ import React from 'react';
 const HeroSection = () => {
   return (
     <section className="hero">
-      <h1>Create, Customize, and Publish Your Digital Persona Effortlessly</h1>
+      <div className='hero-div1'>
+        <button><span>🎉</span><span>New in AI.GEN: Real Time Interaction</span></button>
+      <h1>IOTA polygon serum ipsum WAX terraUSD gala THETA.</h1>
       <button>Get Started</button>
+      </div>
+      <div className='hero-div2'>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+     
     </section>
   );
 };
